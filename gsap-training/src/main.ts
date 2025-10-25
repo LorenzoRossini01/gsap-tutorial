7174d97675401673eec8ed95af8612e58139ec61
LorenzoRossini01 <lory.rox.01@gmail.com>
2025-10-25 11:49:04 +0200

    feat: gsap experimenting to, from, fromTo

---
+++ gsap-training/src/main.ts
@@ -0,0 +1,86 @@
+import "./style.css";
+
+import { gsap } from "gsap";
+// const red = document.querySelector<HTMLElement>(".red");
+// const blue = document.querySelector<HTMLElement>(".blue");
+// const orange = document.querySelector<HTMLElement>(".orange");
+const squares = document.querySelectorAll<HTMLElement>(".square");
+
+// gsap.to(squares, {
+//   x: 500,
+//   y: 200,
+//   scale: 0.5,
+//   rotate: 180,
+//   duration: 1,
+//   delay: 3.5,
+// });
+// gsap.from(squares, {
+//   x: -500,
+//   y: -200,
+//   scale: 20,
+//   rotate: 360,
+//   backgroundColor: "yellow",
+//   duration: 1,
+//   delay: 2.5,
+// });
+
+gsap.fromTo(
+  squares,
+  {
+    autoAlpha: 0,
+    x: 300,
+    scale: 20,
+    rotate: 180,
+  },
+  {
+    autoAlpha: 1,
+    rotate: 0,
+    x: 0,
+    y: 200,
+    scale: 0.5,
+    backgroundColor: "green",
+    delay: 0.5,
+    duration: 1,
+    ease: "elastic",
+  }
+);
+
+// red?.addEventListener("mouseenter", () => {
+//   gsap.fromTo(
+//     red,
+//     {
+//       scale: 1.2,
+//       duration: 1,
+//     },
+//     {
+//       scale: 1,
+//       duration: 1,
+//     }
+//   );
+// });
+// blue?.addEventListener("mouseenter", () => {
+//   gsap.fromTo(
+//     blue,
+//     {
+//       backgroundColor: "#ae7fff",
+//     },
+//     {
+//       backgroundColor: "blue",
+//       duration: 1,
+//     }
+//   );
+// });
+// orange?.addEventListener("mouseenter", () => {
+//   gsap.to(orange, {
+//     rotate: 360,
+//     borderRadius: "50%",
+//     duration: 1,
+//   });
+// });
+// orange?.addEventListener("mouseleave", () => {
+//   gsap.to(orange, {
+//     rotate: -180,
+//     borderRadius: "0",
+//     duration: 1,
+//   });
+// });
